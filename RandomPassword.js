923d1edb09b652954a5726b06189b9fc7e882779
EzraIII <Ezra3Ezra3@gmail.com>
2019-05-23 15:33:17 -0700

    I did a lot of changes, and now all of the needed functionality works

--- RandomPassword.js
+++ RandomPassword.js
@@ -1,47 +1,105 @@
 "use strict";
 
 var password = "";
-var passwordLength, arrayOfCharacters;
+var passwordLength, arrayOfCharacters, spotForUL, spotForLL, spotForNu, spotForSy, theArrayToUse;
 var character = "Error";
+var initialized = false;
+var minLength = 15;
+var maxLength = 15;
+const arrayOfLowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const arrayOfUppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const arrayOfNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const arrayOfSymbols = ["@", "%", "+", "/", "!", "#", "$", "^", "?", ",", "^", "?", ",", "(", ")", "{", "}", "[", "]", "~", "-", "_", "."];
 
-function GenerateRandomPassword(minLength, maxLength) {
+function GenerateRandomPassword() {
+    if(initialized == false) {
+        const minLengthOfPassword = document.getElementById("minLengthOfPassword");
+        const maxLengthOfPassword = document.getElementById("maxLengthOfPassword");
+        const randomPasswordTextField = document.getElementById("RandomPassword");
+        const checkboxRUL = document.getElementById("RUL");
+        const checkboxNUL = document.getElementById("NUL");
+        const checkboxRLL = document.getElementById("RLL");
+        const checkboxNLL = document.getElementById("NLL");
+        const checkboxRN = document.getElementById("RN");
+        const checkboxNN = document.getElementById("NN");
+        const checkboxRS = document.getElementById("RS");
+        const checkboxNS = document.getElementById("NS");
+        initialized = true;
+    }
+    spotForLL = null;
+    spotForUL = null;
+    spotForNu = null;
+    spotForSy = null;
+    if(document.getElementById("minLengthOfPassword").value > 0) minLength = Math.ceil(document.getElementById("minLengthOfPassword").value);
+    if(document.getElementById("maxLengthOfPassword").value > 0) maxLength = Math.ceil(document.getElementById("maxLengthOfPassword").value);
     arrayOfCharacters = [];
-    addLowerCaseCharacters();
+    if(!document.getElementById("NUL").checked) addUppercaseLetters();
+    if(!document.getElementById("NLL").checked) addLowercaseLetters();
+    if(!document.getElementById("NN").checked) addNumbers();
+    if(!document.getElementById("NS").checked) addSymbols();
     password = "";
     passwordLength = Math.floor(Math.random()*(maxLength-minLength+1)+minLength);
-    for (var i = passwordLength; i > 0; i--) {
-        character = arrayOfCharacters[Math.floor(Math.random()*arrayOfCharacters.length)];
+    var charactersUnclaimed = passwordLength;
+    if(document.getElementById("RUL").checked) {
+        spotForUL = Math.floor(Math.random()*charactersUnclaimed);
+        charactersUnclaimed--;
+    }
+    if(document.getElementById("RLL").checked) {
+        spotForLL = Math.floor(Math.random()*charactersUnclaimed);
+        if(spotForLL >= spotForUL) spotForLL++;
+        charactersUnclaimed--;
+    }
+    if(document.getElementById("RN").checked) {
+        spotForNu = Math.floor(Math.random()*charactersUnclaimed);
+        if(spotForNu >= spotForUL || spotForNu >= spotForLL) spotForNu++;
+        if(spotForNu >= spotForUL && spotForNu >= spotForLL) spotForNu++;
+        charactersUnclaimed--;
+    }
+    if(document.getElementById("RS").checked) {
+        spotForSy = Math.floor(Math.random()*charactersUnclaimed);
+        if(spotForSy >= spotForUL || spotForSy >= spotForLL || spotForSy >= spotForNu) spotForSy++;
+        if(spotForSy >= spotForUL && (spotForSy >= spotForLL || spotForSy >= spotForNu) || (spotForSy >= spotForLL && spotForSy >= spotForNu)) spotForSy++;
+        if(spotForSy >= spotForUL && spotForSy >= spotForLL && spotForSy >= spotForNu) spotForSy++;
+    }
+    for (var i = 0; i < passwordLength; i++) {
+        theArrayToUse = arrayOfCharacters;
+        if(i===spotForUL){theArrayToUse = arrayOfUppercaseLetters;}
+        else if(i===spotForLL){theArrayToUse = arrayOfLowercaseLetters;}
+        else if(i===spotForNu){theArrayToUse = arrayOfNumbers;}
+        else if(i===spotForSy){theArrayToUse = arrayOfSymbols;}
+        character = theArrayToUse[Math.floor(Math.random()*theArrayToUse.length)];
         password += character;
     }
-    console.log(password);
-    console.log(passwordLength);
+    document.getElementById("RandomPassword").value = password;
 }
 
-function addLowerCaseCharacters() {
-    arrayOfCharacters.push("a");
-    arrayOfCharacters.push("b");
-    arrayOfCharacters.push("c");
-    arrayOfCharacters.push("d");
-    arrayOfCharacters.push("e");
-    arrayOfCharacters.push("f");
-    arrayOfCharacters.push("g");
-    arrayOfCharacters.push("h");
-    arrayOfCharacters.push("i");
-    arrayOfCharacters.push("j");
-    arrayOfCharacters.push("k");
-    arrayOfCharacters.push("l");
-    arrayOfCharacters.push("m");
-    arrayOfCharacters.push("n");
-    arrayOfCharacters.push("o");
-    arrayOfCharacters.push("p");
-    arrayOfCharacters.push("q");
-    arrayOfCharacters.push("r");
-    arrayOfCharacters.push("s");
-    arrayOfCharacters.push("t");
-    arrayOfCharacters.push("u");
-    arrayOfCharacters.push("v");
-    arrayOfCharacters.push("w");
-    arrayOfCharacters.push("x");
-    arrayOfCharacters.push("y");
-    arrayOfCharacters.push("z");
+function addLowercaseLetters(){
+    for(var i = 0; i < arrayOfLowercaseLetters.length; i++){
+        arrayOfCharacters.push(arrayOfLowercaseLetters[i]);
+    }
 }
+
+function addUppercaseLetters(){
+    for(var i = 0; i < arrayOfUppercaseLetters.length; i++){
+        arrayOfCharacters.push(arrayOfUppercaseLetters[i]);
+    }
+}
+
+function addNumbers(){
+    for(var i = 0; i < arrayOfNumbers.length; i++){
+        arrayOfCharacters.push(arrayOfNumbers[i]);
+    }
+}
+
+function addSymbols(){
+    for(var i = 0; i < arrayOfSymbols.length; i++){
+        arrayOfCharacters.push(arrayOfSymbols[i]);
+    }
+}
+
+/*function copyPassword(){
+    var thePassword = document.getElementById("copyPasswordButton");
+    thePassword.focus();
+//    thePassword.select();
+//    document.execCommand("copy");
+}*/
